a21012a0f0e3d0b36915e6d2d3411ffb8141d0e1
Дмитрий Щербаков <atomcms@ya.ru>
2019-05-21 13:12:52 +0500

    feat Перенос задач на gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,37 @@ var cleanCSS = require('gulp-clean-css');
 var rename   = require('gulp-rename');
 var uglify   = require('gulp-uglify');
 
-gulp.task('default', ['css', 'js'], function () {
-    gulp.watch('src/*.css', ['css']);
-    gulp.watch('src/*.js', ['js']);
-});
-
-gulp.task('build', ['css', 'js'], function () {
-
-});
-
-gulp.task('css', function () {
-    gulp.src('src/jquery.lightajax.css')
+function CSS() {
+    return gulp.src('src/jquery.lightajax.css')
         .pipe(gulp.dest('dist'));
+}
 
-    gulp.src('src/jquery.lightajax.css')
+function JS() {
+    return gulp.src('src/jquery.lightajax.js')
+        .pipe(gulp.dest('dist'));
+}
+
+function minCSS() {
+    return gulp.src('src/jquery.lightajax.css')
         .pipe(cleanCSS())
         .pipe(rename('jquery.lightajax.min.css'))
         .pipe(gulp.dest('dist'));
-});
+}
 
-gulp.task('js', function () {
-    gulp.src('src/jquery.lightajax.js')
-        .pipe(gulp.dest('dist'));
-
-    gulp.src('src/jquery.lightajax.js')
+function minJS() {
+    return gulp.src('src/jquery.lightajax.js')
         .pipe(uglify())
         .pipe(rename('jquery.lightajax.min.js'))
         .pipe(gulp.dest('dist'));
-});
+}
+
+gulp.task('build', gulp.series(
+    gulp.parallel(
+        CSS,
+        JS
+    ),
+    gulp.parallel(
+        minCSS,
+        minJS
+    )
+));
